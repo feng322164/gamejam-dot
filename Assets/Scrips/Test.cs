ec2692161276570022346b72d6702cf82a7fa96d
沃布时沙雕\fjh <3124256916@qq.com>
2026-02-03 12:27:11 +0800

    测试

---
+++ Assets/Scrips/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("Hello word!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
